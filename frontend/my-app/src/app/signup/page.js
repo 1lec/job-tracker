c504373e0e94b9642b6dd67dd1103fc5cb835227
alec <alecandersen97@gmail.com>
2025-04-24 14:36:17 -0400

    Finish signup page

--- frontend/my-app/src/app/signup/page.js
+++ frontend/my-app/src/app/signup/page.js
@@ -8,7 +8,26 @@ export default function HelloPage() {
 
   return (
     <main>
-      <h1>Hello, world!</h1>
+      <h1>Signup</h1><br></br>
+      <p>New to Job Tracker? Complete the form to create your account and optimize your job search!</p><br></br>
+      <form>
+        <label htmlFor="first_name" required>First Name:</label><br></br>
+        <input type="text"></input><br></br>
+
+        <label htmlFor="last_name" required>Last Name:</label><br></br>
+        <input type="text"></input><br></br>
+
+        <label htmlFor="email" required>Email Address:</label><br></br>
+        <input type="email"></input><br></br>
+
+        <label htmlFor="password" required>Password:</label><br></br>
+        <input type="password"></input><br></br>
+
+        <label htmlFor="skills" required>Skills:</label><br></br>
+        <input type="text"></input><br></br><br></br>
+
+        <input type="submit" value="Sign Up"></input>
+      </form>
       <button
         onClick={() => router.push('/')}
         style={{
