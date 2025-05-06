7c266376f8b66141a87d1bbd50d608e1640feb04
alec <alecandersen97@gmail.com>
2025-05-06 18:39:42 -0400

    Create Login and Signup endpoints within a controller

---
+++ backend/Models/Authentication/SignupRequest.cs
@@ -0,0 +1,10 @@
+namespace JobTracker.Backend.Models.Auth;
+
+public class SignupRequest
+{
+    public string FirstName { get; set; } = "";
+    public string LastName { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string Password { get; set; } = "";
+    public List<string> Skills { get; set; } = new();
+}
